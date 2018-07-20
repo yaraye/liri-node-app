9002588d81337d2ee9650c404cf6294ad17e6225
Yaraye <yaraye@gmail.com>
2018-07-20 17:46:46 -0400

    updates

--- liri.js
+++ liri.js
@@ -1,70 +1,81 @@
+require("dotenv").config(); 
+
+//variables 
 var keys = require("./keys.js");
 
-//node liri.js movie-this '<movie name here>'`
-var request = require('request');
-var movies = process.argv;
+var fs = require("fs");
 
-// if(process.argv[2] == "movie-this")
-// {
-//   movieThis();
-// }
-// else if(process.argv[2] == "twitter-this")
-// {
+var twitter = require("twitter");
+var client = new twitter(keys.twitter);
 
-// }
+var spotify = require("node-spotify-api");
+var spotify = new spotify(keys.spotify);
 
-function movieThis(){
-   
-  var movieName = "";
-// Loop through all the words in the node argument
-for (var i = 3; i < movies.length; i++) {
-    movieName = movieName + "  " + process.argv[i];
-}
-console.log(movieName);
+var request = require("request");
+var movieName = process.argv[3];
 
-if(movieName == "")
+var liriReturn = process.argv[2];
+
+
+//switches case
+
+switch(liriReturn)
 {
-  movieName = "Nobody";
-}
- 
 
-// Then run a request to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-request(queryUrl, function(error, response, body) {
-
-  // If the request is successful
-//   console.log(JSON.stringify(body, null, 2));
-  if (!error && response.statusCode === 200)
-  {
-      var movieObject = JSON.parse(body);
-
-    console.log("Title: " + movieObject.Title);
-    console.log("Year: " + movieObject.Year);
-    console.log("IMDB Rating: " + movieObject.imdbRating);
-    console.log("country where movie was produce: " + movieObject.Country);
-    console.log("Language: " + movieObject.Language);
-    console.log("Plot: " + movieObject.plot);
-    console.log("Actors: " + movieObject.Actors);
-
-  }
-  else {
-    console.log("Error :"+ error);
-    return;
-
-  }
-  function logInfo(movie) {
-	// Append the command to the log file
-	fs.appendFile('./log.txt', 'User Command: node liri.js movie-this ' + movie ,function(error)
-       {
-		if (error) throw error;
-    });
-}
-});
+  case 'my-tweets':
+  myTweets();
+  break;
   
+  case "spotify-this-song":
+  spotifyThisSong();
+  break;
+
+  case 'movie-this':
+  movieThis();
+  break;
+
+  case 'do-what-it-says':
+  doWhatItSaya();
+  break;
+};
+
+
+//tweets
+function myTweets(){
+
+var params = {screen_name: 'Ynode'};
+
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    for(var i=0; i < tweets.length; i++){
+      console.log(tweets[i].text);
+    }
+  }
+    else {
+      console.log("Error:" +error);
+      return;
+    }
+});
 }
 
+//spotify
+function spotifyThisSong(song){
 
+  var song="halo";
 
-console.log("------------------------------------------------------------------------------------------------")
+  spotify.search({ type: 'track', query: song }, function(err, data) {
+    if (!err) {
+console.log(song);
+      var songInfo = data.track.items;
 
+      for (var i=0; i < songInfo.length; i++){
+        console.log("Artist:" + songInfo[i].name)
+      }
+    }else{
+      console.log('Error occurred: ' + err);
+      return;
+    }
+   
+  
+  });
+};
