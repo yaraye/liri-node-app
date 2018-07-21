8aefb4c01aaebbffbb714670381dbf9f86f33666
Yaraye <yaraye@gmail.com>
2018-07-20 21:52:34 -0400

    updates

--- liri.js
+++ liri.js
@@ -65,12 +65,17 @@ function spotifyThisSong(song){
 
   spotify.search({ type: 'track', query: song }, function(err, data) {
     if (!err) {
-console.log(song);
-      var songInfo = data.track.items;
+//console.log(JSON.stringify(data,null,2));
+     
+var songs = data.tracks.items;
 
-      for (var i=0; i < songInfo.length; i++){
-        console.log("Artist:" + songInfo[i].name)
-      }
+for (var i = 0; i < songs.length; i++) {
+
+  console.log("album: " + songs[i].album.name);
+  console.log("-----------------------------------");
+}
+
+
     }else{
       console.log('Error occurred: ' + err);
       return;
@@ -79,3 +84,51 @@ console.log(song);
   
   });
 };
+
+// //movie
+var movies = process.argv[3];
+
+
+function movieThis(){
+    
+    if(!movies){
+        movies = "mr nobody";
+    }
+}
+//empty variable for holding the movie name
+var movieName = "";
+
+// Loop through all the words in the node argument
+for (var i = 3; i < process.argv.length; i++) {
+
+    movieName = movieName + "+" + process.argv[i];
+
+}
+
+// console.log(movieName);
+// Then run a request to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+request(queryUrl, function(error, response, body) {
+
+  // If the request is successful
+
+  console.log(JSON.stringify(body, null, 2));
+  if (!error && response.statusCode === 200)
+  {
+      var movieObject = JSON.parse(body);
+
+    console.log("Title: " + movieObject.Title);
+    console.log("Year: " + movieObject.Year);
+    console.log("IMDB Rating: " + movieObject.imdbRating);
+    console.log("country where movie was produce: " + movieObject.Country);
+    console.log("Language: " + movieObject.Language);
+    console.log("Plot: " + movieObject.plot);
+    console.log("Actors: " + movieObject.Actors);
+  }
+  else {
+    console.log("Error :"+ error);
+    return;
+
+  }
+});
